d422338dedbd485396aba4018b47f0d323946753
Colin Murtaugh <cmurtaugh@gmail.com>
2020-05-03 08:48:56 -0400

    update to instantclient version 19.6

--- files/oracle_home.sh
+++ files/oracle_home.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-export ORACLE_HOME=/opt/oracle/instantclient_11_2
-export LD_LIBRARY_PATH=/opt/oracle/instantclient_11_2
+export ORACLE_HOME=/opt/oracle/instantclient_19_6
+export LD_LIBRARY_PATH=/opt/oracle/instantclient_19_6
